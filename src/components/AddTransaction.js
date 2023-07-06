fec489ee072a928917a0292b780e16589f7693ef
Noorul Ashkar <noorulashkar.n@infosys.com>
2023-07-06 17:56:03 +0530

    first commit

---
+++ src/components/AddTransaction.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const AddTransaction = () => {
+  return (
+    <>
+    <h3>Add new transaction</h3>
+    h3  
+    <form>
+        
+    </form>
+    </>
+  )
+}
