7d20bb8818c187465a318e02f1f3f6316a511bbe
Noorul Ashkar <noorulashkar.n@infosys.com>
2023-07-07 11:11:17 +0530

    second commit

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -4,9 +4,9 @@ export const TransactionList = () => {
   return (
     <>
     <h3>History</h3>
-    <ul id="list" class="list">
-        <li class="minus">
-            Cash<span>-$400</span><button class="delete-btn"></button>
+    <ul className="list">
+        <li className="minus">
+            Cash<span>-$400</span><button className="delete-btn"></button>
         </li>
     </ul>
     </>
